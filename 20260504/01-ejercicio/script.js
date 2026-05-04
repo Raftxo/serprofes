98712b999e40750d5f911c8896e0a4dda3de9bac
Raftxo <raftxo@raftxo-asus-mx>
2026-05-04 21:41:41 +0200

    First exercises with javascript

---
+++ 20260504/01-ejercicio/script.js
@@ -0,0 +1,13 @@
+console.log("¡El cerebro de la web está funcionando!");
+
+function cerrarVentana(){
+    self.close();
+}
+
+function saludar(){
+    console.log("¡No me toques!");
+}
+
+function verificarAcceso(){
+    console.log("¡Tú no entras!")
+}
