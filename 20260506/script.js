e52b17b955c2f9ae07a12bab0b194430ac487174
Raftxo <fatwhiteduke@gmail.com>
2026-05-07 18:45:21 +0200

    evaluador de notas javascript

--- 20260506/script.js
+++ 20260506/script.js
@@ -1,19 +1,17 @@
-function comprobarNumero(){
-    // atrapamos el número usando let y getElementById
-    let numero=Number(document.getElementById('numero-input').value);
-    let etiqueta=document.getElementById('mensaje-salida');
+function comprobarNumero() {
+  // atrapamos el número usando let y getElementById
+  let numero = Number(document.getElementById("numero-input").value);
+  let etiqueta = document.getElementById("mensaje-salida");
 
-    // tomamos la decisión con IF / ELSE
-    if (numero >= 0 && numero <= 4) {
-        // ...
-        etiqueta.textContent = "Tu nota: " + numero + ". Has suspendido.";
-        etiqueta.style.color = "red";
-    } else if (numero >= 5 && numero <= 10) {
-        // y si NO ...
-        etiqueta.textContent = "Tu nota: " + numero + ". Has aprobado.";
-        etiqueta.style.color = "green";
-    } else {
-        etiqueta.textContent = "Tu nota: " + numero + " no es número válido.";
-        etiqueta.style.color = "blue";
-    }
+  // tomamos la decisión con IF / ELSE
+  if (numero === "" || numero < 0 || numero > 10) {
+    etiqueta.textContent = "Introduce un número válido entre 0 y 10.";
+    etiqueta.style.color = "blue";
+  } else if (numero >= 0 && numero <= 4) {
+    etiqueta.textContent = "Tu nota: " + numero + ". Has suspendido.";
+    etiqueta.style.color = "red";
+  } else {
+    etiqueta.textContent = "Tu nota: " + numero + ". Has aprobado.";
+    etiqueta.style.color = "green";
+  }
 }
