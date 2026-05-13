78bb279ed4fdb775706c1647c1ee40bf4ec73d95
Raftxo <fatwhiteduke@gmail.com>
2026-05-13 19:40:22 +0200

    add shopping cart as an array in javascript

--- 20260512/02-ejercicio/script.js
+++ 20260512/02-ejercicio/script.js
@@ -1,1 +1,9 @@
-// no script
+// nuestro súper array lleno de objetos
+const carrito = [
+    {nombre: "🥖 Pan de Masa Madre", precio:1.20},
+    {nombre: "🧃 Leche Entera", precio:0.90},
+    {nombre: "🥚 Huevos Camperos", precio:2.50},
+    {nombre: "🥑 Aguacate",precio:1.00},
+    {nombre: "🧄 Ajo",precio:0.55},
+    {nombre: "🍾 Agua con Gas",precio:0.75}
+];
