3a6dcc0f3a1c82d3304472a261722567aa0bd7fc
Raftxo <fatwhiteduke@gmail.com>
2026-05-14 21:56:14 +0200

    add painting chat function into app.js of the saas-ia project

--- saas-ia/app.js
+++ saas-ia/app.js
@@ -1,3 +1,30 @@
+// creamos una "base de datos" MOCK
+let historialChat = [
+    {rol:"ia",texto:"¡Hola! Soy IA Master. ¿En qué te ayudo?"},
+    {rol:"user",texto:"Quiero aprender JavaScript"},
+    {rol:"ia",texto:"¡Excelente elección! Empezaremos por los Arrays."},
+];
+
+// función para ver ese chat en mi página
+function pintarChat(listaMensajes){
+    let caja = document.getElementById('caja-mensajes');
+    caja.innerHTML = "";
+    // mantenemos el bucle FOR
+    for(let i = 0;1<listaMensajes.lenght;i++){
+        let claseCSS = listaMensajes[i].rol === "user" ? "msg-usuario" : "msg-ia";
+        caja.innerHTML += `
+            <div class="${claseCSS}"><b>
+            ${listaMensajes[i].rol.toUpperCase()}:</b>
+            <br>
+            ${listaMensajes[i].texto}</div>
+        `;
+    }
+}
+
+
+
+
+
 function enviarPrompt(event){
     // evitamos que el form recargue la página cada vez que pulsamos el botón Enviar
     event.preventDefault();
@@ -14,7 +41,4 @@ function enviarPrompt(event){
         document.getElementById("mensaje-input").value = "";
     
     };
-
-
-
 }
