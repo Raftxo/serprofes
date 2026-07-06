41efce62015c5af7973a7238738126084dab6da6
Raftxo <fatwhiteduke@gmail.com>
2026-07-06 20:53:42 +0200

    update hb-jose-backend server.js (running and listening on port 3000)

--- 20260706-hb-jose-backend/server.js
+++ 20260706-hb-jose-backend/server.js
@@ -9,3 +9,16 @@ const inventario = [
     {id: 2, articulo: "Monitor Dell", stock: 5},
     {id: 3, articulo: "Teclado Mecánico", stock: 22}
 ];
+// 3. LA RUTA (Camarero)
+//    Cuando alguien pida 'api/productos', le entregamos el inventario
+
+app.get('/api/productos', (req, res) => {
+    // res.json convierte los datos para que el internet entienda
+    res.json(inventario);
+});
+
+// 4. Encender el servidor
+//    Le decimos que escuche en el puerto 3000
+app.listen(3000, () => {
+    console.log('🏃‍♀️ Servidor encendido y escuchando en el puerto 3000');
+});
